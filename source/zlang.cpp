3252b812d4c375368635d994b4347e1c207d2096
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-16 15:31:48 +0000

    Add documentation and links to main file (source/zlang.cpp)

---
+++ source/zlang.cpp
@@ -0,0 +1,8 @@
+// The zlang compiler project uses a
+// frontend made in C++ and the LLVM
+// backend framework
+// https://llvm.org/
+
+// Visit the LLVM kaleidoscope tutorial:
+// https://llvm.org/docs/tutorial/index.html
+// for frontend compiler creation tutorial
