525e5191b90b80e47186f6b49f42eea7d996b62f
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-02-16 15:20:12 +0000

    Add gettok function to tokens module (source/token.hpp)

--- source/token.hpp
+++ source/token.hpp
@@ -1,7 +1,9 @@
 #ifndef __ZLANG_TOKEN_HPP__
 #define __ZLANG_TOKEN_HPP__
 
+#include <fstream>
 #include <string>
+#include <cctype>
 
 typedef enum __ZZ_TOKEN__ {
 	/* EOF token */
@@ -16,6 +18,44 @@ typedef enum __ZZ_TOKEN__ {
 	zznumber     = -5, // for example: 2
 	zzstring     = -6  // for example: "Hello"
 } zztoken;
+
+
+static std::string identifier;
+static double      number;
+
+
+static zztoken gettok(std::fstream file) {
+	static int last_char = ' ';
+
+	while (isspace(last_char))
+		file.get(last_char);
+
+	if (isalpha(last_char)) {
+		identifier = last_char;
+		file.get(last_char);
+		while (isalnum(last_char)) {
+			identifier += last_char;
+			file.get(last_char);
+		}
+
+		if (identifier == "fun") // function
+			return zzfun;
+		else if (identifier == "ext") // extern function
+			return zzext;
+
+		return zzidentifier;
+
+	} else if (isdigit(last_char) || last_char == '.') {
+		std::string number_str;
+		do {
+			number_str += last_char;
+			file.get(last_char);
+		} while (isdigit(last_char) || last_char == '.');
+
+		number = strtod(number_str.c_str(), 0);
+		return zznumber;
+	}
+}
 
 
 #endif /* !__ZLANG_TOKEN_HPP__ */
